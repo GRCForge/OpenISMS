b5ef8925c8084d1128861b369f7add4a5b0f4e4c
Maximilian Herz <herzmaximilian@gmail.com>
2026-06-22 19:46:21 +0000

    Localize Pentests page and set up pentests namespace

--- frontend/src/i18n.ts
+++ frontend/src/i18n.ts
@@ -13,6 +13,7 @@ const NAMESPACES = [
   'c5', 'bsigrundschutz', 'iso27001', 'nis2',
   'subjectrequests', 'myarea', 'aiact', 'bcm', 'dora',
   'networkdiscovery', 'tisax', 'policylibrary', 'managementreport',
+  'pentests',
 ];
 
 i18n
